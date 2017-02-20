1ab79fe352e2ce8357996af3d3ea41475bcb63dd
tobster30 <tobster50@outlook.com>
2017-02-20 11:23:56 +0000

    Create GUImain.java

---
+++ GUImain.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+
+
+public class mainClass {
+	
+	public static void main(String[] args) {
+		
+		JFrame myFrame = new JFrame ("Welcome to week 4 tutorials");
+		
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//myFrame.pack();
+		
+		myFrame.setVisible(true);
+		myFrame.setSize(200,200);
+		
+	}
+
+}
